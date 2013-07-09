1a5a7ae9b17deb624baf6ba72e59a44540879067
ThomasEding <thomas@techsoft3d.com>
2013-07-09 11:27:10 -0700

    initial

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main (
+      main
+    ) where
+
+
+import qualified SplitOn
+
+
+main :: IO ()
+main = SplitOn.main
+
+
---
+++ src/SplitOn.hs
@@ -0,0 +1,99 @@
+module SplitOn (
+      main
+    ) where
+
+
+import Control.Monad
+import Data.List
+import Data.List.Split
+import System.Environment
+import System.Exit
+
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let opts = parseArgs args
+    when (oHelp opts) helpIO
+    case oSplitDelim opts of
+        Nothing -> helpIO
+        Just splitDelim -> case oJoinDelim opts of
+            Nothing -> helpIO
+            Just joinDelim -> splitIO splitDelim joinDelim
+
+
+helpIO :: IO ()
+helpIO = do
+    putStrLn "TODO: Help display"
+    exitSuccess
+
+
+splitIO :: String -> String -> IO ()
+splitIO splitDelim joinDelim = do
+    contents <- getContents
+    let chunks = splitOn splitDelim contents
+        res = intercalate joinDelim chunks
+    putStrLn res
+
+
+data Options = Options {
+      oHelp :: Bool
+    , oSplitDelim :: Maybe String
+    , oJoinDelim :: Maybe String
+    }
+
+
+parseArgs :: [String] -> Options
+parseArgs = parseArgs' opts
+    where
+        opts = Options {
+              oHelp = False
+            , oSplitDelim = Nothing
+            , oJoinDelim = Nothing
+            }
+
+
+parseArgs' :: Options -> [String] -> Options
+parseArgs' opts args = case args of
+    [] -> opts
+    "-h" : _ -> opts{ oHelp = True }
+    "--help" : _ -> opts{ oHelp = True }
+    "-s" : delim : args' -> parseArgs' opts{ oSplitDelim = Just $ decode delim } args'
+    "--split" : delim : args' -> parseArgs' opts{ oSplitDelim = Just $ decode delim } args'
+    "-j" : delim : args' -> parseArgs' opts{ oJoinDelim = Just $ decode delim } args'
+    "--join" : delim : args' -> parseArgs' opts{ oJoinDelim = Just $decode delim } args'
+
+
+decode :: String -> String
+decode str = case str of
+    "" -> ""
+    '\\' : c : rest -> unescape c : decode rest
+    "\\" -> error "Cannot decode: Bad escape sequence"
+    c : rest -> c : decode rest
+
+
+unescape :: Char -> Char
+unescape c = case c of
+    'n' -> '\n'
+    'r' -> '\r'
+    't' -> '\t'
+    'v' -> '\v'
+    '\\' -> '\\'
+    '\'' -> '\''
+    '"' -> '"'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
